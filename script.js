f088bbf22239406456c35f6100d155be5c341355
Rachel <eedoudou@users.noreply.github.com>
2023-05-05 20:45:04 -0400

    add jumbotron updates, add humidity etc updates

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-let api = "https://api.openweathermap.org/data/2.5/forecast"
-let units = "metric"
-let iconClass = 'wi wi-owm-day-'; // weather icons
+let forecastApi = "https://api.openweathermap.org/data/2.5/forecast";
+let weatherApi = "https://api.openweathermap.org/data/2.5/weather";
+
 let city;
 let country;
-let bg;
-let date;
 let appid = "5be662184a5f66d241c04ec58eab34ec";
 
 let day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let gifSrc = "src/gif/";
+let gifDict = {
+    "2": "storm.gif",
+    "3": "rainy.gif",
+    "5": "rainy.gif",
+    "6": "rainy.gif",
+    "7": "sunny.gif",
+    "8": "cloudy.gif",
+}
+let materialDict = {
+    "2": "Thunderstorm",
+    "3": "Rainy",
+    "5": "Rainy",
+    "6": "Rainy",
+    "7": "Sunny",
+    "8": "Cloudy",
+}
 
 let updateTime = function () {
     let date = new Date();
@@ -20,55 +36,62 @@ let updateTime = function () {
 updateTime();
 setInterval(updateTime, 1000);
 
-function setWeather(w) {
-    const weather = w.weather[0];
-    if (date.getHours() >= 18 || date.getHours() <= 6) {
-        iconClass = 'wi wi-owm-night-';
-    }
-    var temp = Math.round(w.main.temp), pressure = w.main.pressure, humid = w.main.humidity, wind = w.wind.speed;
-
-    $('#place').text(city + ', ' + country);
-    $('#temp span').text(temp);
-    $('#icon').removeClass().addClass(iconClass + weather.id);
-
-    $('#p span').text(pressure);
-    $('#h span').text(humid);
-    $('#w span').text(wind);
-    $('.description').text(weather.description);
-}
-
 function getWeather(lat, lon){
-    let newApi = api + "?lat=" + lat + "&lon=" + lon + "&appid=" + appid + "&units=metric";
-    $.getJSON(newApi, function (obj) {
-        console.log(obj)
+    let curWeatherApi = weatherApi + "?lat=" + lat + "&lon=" + lon + "&appid=" + appid + "&units=metric";
+    $.getJSON(curWeatherApi, function (data) {
+        console.log(data);
 
-        let cityName = obj["city"]["name"];
-        let country = obj["city"]["country"];
+        document.getElementById("current-temp").innerText = Math.round(data["main"]["temp"]);
+        document.getElementById("current-humidity").innerText = data["main"]["humidity"] + "%";
+        document.getElementById("current-feelslike").innerText = Math.round(data["main"]["feels_like"]) + "°C";
+
+        let weather = data["weather"][0]["id"];
+        let firstDigitStr = String(weather)[0];
+        let hero = document.getElementsByClassName("hero")[0];
+        let $weather = document.getElementById("current-weather");
+        switch (firstDigitStr){
+            case "8":
+                if (weather == "800") {
+                    hero.style.backgroundImage = "url(" + gifSrc + gifDict["7"] + ")";
+                    $weather.innerText = materialDict["7"];
+                } else {
+                    hero.style.backgroundImage = "url(" + gifSrc + gifDict["8"] + ")";
+                    $weather.innerText = materialDict["8"];
+                }
+                break;
+            default:
+                hero.style.backgroundImage = "url(" + gifSrc + gifDict[firstDigitStr] + ")";
+                $weather.innerText = materialDict[firstDigitStr];
+        }
+
+    });
+    let curForecastApi = forecastApi + "?lat=" + lat + "&lon=" + lon + "&appid=" + appid + "&units=metric";
+    $.getJSON(curForecastApi, function (data) {
+        console.log(data)
+
+        let cityName = data["city"]["name"];
+        let country = data["city"]["country"];
         document.getElementById("address").innerText = cityName + ", " + country;
 
-        let sunrise = new Date((obj["city"]["sunrise"]) * 1000);
+        let sunrise = new Date((data["city"]["sunrise"]) * 1000);
         let sunriseTime =  sunrise.getHours() + ":" + (sunrise.getMinutes()<10?'0':'') + sunrise.getMinutes();
-        let sunset = new Date((obj["city"]["sunset"]) * 1000);
+        let sunset = new Date((data["city"]["sunset"]) * 1000);
         let sunsetTime =  sunset.getHours() + ":" + (sunset.getMinutes()<10?'0':'') + sunset.getMinutes();
 
         document.getElementById("sunset-time-major").innerText = "Today's Sunset Time " + sunsetTime;
         document.getElementById("sunrise-time-minor").innerText = sunriseTime;
         document.getElementById("sunset-time-minor").innerText = sunsetTime;
 
-        document.getElementById("current-temp").innerText = Math.round(obj["list"][0]["main"]["temp"]);
-        document.getElementById("current-humidity").innerText = obj["list"][0]["main"]["humidity"] + "%";
-        document.getElementById("current-feelslike").innerText = Math.round(obj["list"][0]["main"]["feels_like"]) + "°C";
+
         let weatherList = [];
-        for (let i = 0; i < obj["list"].length; i ++){
+        for (let i = 0; i < data["list"].length; i ++){
             let cur = {};
-            cur.date = new Date(obj["list"][i]["dt"] * 1000);
-            cur.humidity = obj["list"][i]
+            cur.date = new Date(data["list"][i]["dt"] * 1000);
+            cur.humidity = data["list"][i]
 
             weatherList.push(cur);
         }
 
-
-        setWeather(obj);
     });
 }
 
@@ -101,13 +124,17 @@ function drawChart() {
     var options = {
         curveType: 'function',
         legend: {position: 'none'},
-        backgroundColor: { fill:'transparent' },
-        hAxis: { baselineColor: 'none', textPosition: 'none', gridlines: {
+        backgroundColor: {fill: 'transparent'},
+        hAxis: {
+            baselineColor: 'none', textPosition: 'none', gridlines: {
                 color: 'transparent'
-            } },
-        vAxis: { textPosition: 'none', gridlines: {
+            }
+        },
+        vAxis: {
+            textPosition: 'none', gridlines: {
                 color: 'transparent'
-            } },
+            }
+        },
         gridlines: {
             color: 'transparent'
         }
